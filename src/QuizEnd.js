e169096f2be71149a7e17fb1187cef04105eb1ee
Shamil GHASEETA <shamil.ghaseeta.c@thalesdigital.io>
2018-08-31 14:13:35 +0200

    Completed module 2

---
+++ src/QuizEnd.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class QuizEnd extends Component {
+  render() {
+    return (
+<div>
+        <p>Thanks for playing!</p>
+        <a href=''>Reset Quiz</a>
+      </div>
+    )
+  }
+}
